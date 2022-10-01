85a7aba247c45aba477b1be304cf96f388888e62
R-Grigala <r.grigalashvili777@gmail.com>
2022-10-01 22:04:47 +0400

    Email complated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -9,51 +9,45 @@ import 'react-toastify/dist/ReactToastify.css'
 export const ContactUs = () => {
   const form = useRef();
 
-  const success = () => {
-    toast.success('Email has been sent successfully', {
-      position: "top-right",
-      theme: "dark",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      });
-  }
-
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_0vn7ppa', 'template_sxkp2cn', form.current, 'rbK2QReL4stHDFvXV')
       .then((result) => {
-        success();
+        toast.success('Email has been sent successfully', {
+          position: "top-right",
+          theme: "dark",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
         e.target.reset();
       }, (error) => {
-          toast.error('Email dont send')
+        toast.error("Dont sent yuor email", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
       });
   };
 
   return (
     <>
-    <ToastContainer
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
+    <ToastContainer />
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" placeholder="Type your Name here"/>
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" placeholder="Type your Email here"/>
       <label>Message</label>
-      <textarea name="message" rows="6" placeholder="Type your message here"/>
+      <textarea name="message" rows="6" placeholder="Type your Message here"/>
       <input className="btn" type="submit" value="Send" />
     </form>
 
